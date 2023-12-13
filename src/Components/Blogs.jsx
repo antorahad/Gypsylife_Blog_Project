ecce2562eb78e1129a01f4f8e926120a963052d8
antorahad <antorahad6@gmail.com>
2023-12-13 23:09:22 +0600

    All blogs

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+
 const Blogs = () => {
+    const [categoryButtons, setCategoryButtons] = useState([])
+    useEffect(() => {
+        fetch('/categories.json')
+        .then(res => res.json())
+        .then(data => setCategoryButtons(data))
+    }, [])
     return (
-        <div className="py-40">
-            
+        <div className="px-5">
+            <div className="container mx-auto py-40">
+                <div className="flex items-center justify-center gap-5">
+                    {
+                        categoryButtons.map(categoryButton => <button className="btn bg-red-500 hover:bg-red-500 focus:bg-red-500 text-white py-3 px-5 border-none outline-none" key={categoryButton.id}>{categoryButton.category_name}</button>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,33 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaRedditAlien } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-black">
+            <footer className="container mx-auto p-5 flex flex-col lg:flex-row items-center justify-between gap-5">
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <h1 className="text-2xl font-bold text-white">
+                        <span className="text-red-500 font-lobster">Gypsy</span>Life
+                    </h1>
+                    <p className="text-white text-xs font-light">Travel, Explore and Share</p>
+                </div>
+                <div>
+                    <p className="text-white text-xs font-light">Copyright © 2023 - All right reserved by GypsyLife.</p>
+                </div>
+                <div className="flex items-center justify-center gap-5 text-white">
+                    <Link>
+                        <FaFacebook size={20}></FaFacebook>
+                    </Link>
+                    <Link>
+                        <FaInstagram size={20}></FaInstagram>
+                    </Link>
+                    <Link>
+                        <FaTwitter size={20}></FaTwitter>
+                    </Link>
+                    <Link>
+                        <FaRedditAlien size={20}></FaRedditAlien>
+                    </Link>
+                </div>
+            </footer>
         </div>
     );
 };
