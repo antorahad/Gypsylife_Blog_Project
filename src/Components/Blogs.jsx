5b2759c1293f35a045a16b57545c7e920280bdc4
antorahad <antorahad6@gmail.com>
2023-12-16 13:44:31 +0600

    update

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import error404 from '../assets/404-removebg-preview.png'
 const Blogs = () => {
   const [categories, setCategories] = useState([]);
   const [blogData, setBlogData] = useState([]);
@@ -54,12 +54,18 @@ const Blogs = () => {
     <div className="py-20 px-5">
       <div className="container mx-auto">
         <div className="flex flex-wrap lg:flex-nowrap items-center justify-center gap-3">
-          <button
-            className={selectedCategory === '' ? "btn bg-red-500 hover:bg-red-500 focus:bg-red-500 text-white border-none outline-none rounded-md" : "btn bg-slate-100 hover:bg-slate-100 focus:bg-slate-100 text-slate-950 border-none outline-none rounded-md"}
-            onClick={() => handleCategoryClick('')}
-          >
-            All
-          </button>
+          {displayData.length > 0 && (
+            <button
+              className={
+                selectedCategory === ""
+                  ? "btn bg-red-500 hover:bg-red-500 focus:bg-red-500 text-white border-none outline-none rounded-md"
+                  : "btn bg-slate-100 hover:bg-slate-100 focus:bg-slate-100 text-slate-950 border-none outline-none rounded-md"
+              }
+              onClick={() => handleCategoryClick("")}
+            >
+              All
+            </button>
+          )}
           {categories.map((category, index) => (
             <button
               className={setButtonStyle(category)}
@@ -70,9 +76,18 @@ const Blogs = () => {
             </button>
           ))}
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-5 mt-10">
-          {displayData.length > 0 ? (
-            displayData.map(blog => (
+        {displayData.length === 0 && (
+          <div className="flex items-center justify-center min-h-screen mt-5">
+            <img
+              src={error404}
+              alt="No data available"
+              className="w-[400px] h-[400px] object-cover rounded-md"
+            />
+          </div>
+        )}
+        {displayData.length > 0 && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-5 mt-10">
+            {displayData.map(blog => (
               <div key={blog.id} className="card bg-slate-50 shadow-sm rounded-md w-full p-5">
                 <div className="flex flex-col gap-3">
                   <img src={blog.image} alt={blog.title} className="w-full h-[200px] object-cover rounded-md" />
@@ -84,12 +99,10 @@ const Blogs = () => {
                   </Link>
                 </div>
               </div>
-            ))
-          ) : (
-            <p>No data available</p>
-          )}
-        </div>
-        {selectedCategory === '' && displayData.length > 0 && (
+            ))}
+          </div>
+        )}
+        {selectedCategory === "" && displayData.length > 0 && (
           <div className="flex justify-center mt-7">
             {showAll ? (
               <button
