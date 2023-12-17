d775a75669ba87bdd472957c91731a676cd1d019
antorahad <antorahad6@gmail.com>
2023-12-17 23:31:49 +0600

    error page update

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,8 +1,19 @@
-import error404 from '../assets/404-removebg-preview.png'
+import { Link } from 'react-router-dom';
+import error404 from '../assets/No-data.jpg'
+import { Helmet } from 'react-helmet-async'
 const Error = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <img src={error404} className="w-[500px] h-[500px]" />
+        <div className="min-h-screen flex flex-col items-center gap-3 justify-center">
+            <Helmet>
+                <title>404 | Error Page</title>
+            </Helmet>
+            <div>
+                <img src={error404} className="w-[300px] h-[300px]" />
+            </div>
+            <p className='text-slate-800 font-poppins font-medium'>Sorry! This page is not Found.</p>
+            <Link to={'/'}>
+                <a className='text-green-500 underline'>Return To Home</a>
+            </Link>
         </div>
     );
 };
